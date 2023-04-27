22e79aa50ab1910d84a2973f8a3db6edd12f5002
Mathis Zerbib <33841749+MathisZerbib@users.noreply.github.com>
2023-04-27 20:19:15 +0200

    Update viewer.js GPT4

--- viewer.js
+++ viewer.js
@@ -1,71 +1,55 @@
-const Nightmare = require('nightmare')
-var ProxyLists = require('proxy-lists');
+const Nightmare = require('nightmare');
+const ProxyLists = require('proxy-lists');
 
-var options = { countries: ['fr', 'ca', 'ch','be']};
-var gettingProxies = ProxyLists.getProxies(options);
-
-var P = [];
-var status = true;
-var views = 100;
-var multiplicator = 1;
-var debit = 4500;
-var url = "https://www.google.com";
-var timer = 3000; // random 1000 - x
-
-gettingProxies.on('data', function(proxies) {
-    if(status === true && P.length > views) {
-        status = false;
-        getviews(P);
-    }
-    else if (status === true) {
-        P = P.concat(proxies);
-        console.log("Proxies: ", P.length);
-    }
-});
-gettingProxies.on('error', function(error) { });
-gettingProxies.once('end', function() { });
+const options = { countries: ['fr', 'ca', 'ch', 'be'] };
+const views = 100;
+const multiplicator = 1;
+const debit = 4500;
+const url = 'https://www.google.com';
+const timer = 3000;
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-async function execview(nightmare, id) {
-    console.log("start viewer " + id);
-    try {
-        await nightmare.goto(url);
-        console.log("goto done");
-        let wait = await nightmare.wait(1000 + getRandomInt(timer));
-        console.log("wait 5s done");
-        let end = await nightmare.end();
-    } catch (e) {
-        console.log("error: ", e);
-        let end = await nightmare.end();
+async function execView(proxy, id) {
+  console.log(`Start viewer ${id} with proxy ${proxy.ipAddress}:${proxy.port}`);
+  const nightmare = Nightmare({ show: false, switches: {
+    'proxy-server': `${proxy.ipAddress}:${proxy.port}`,
+    'ignore-certificate-errors': true
+  }});
+  try {
+    await nightmare.goto(url);
+    console.log(`Goto done for viewer ${id}`);
+    await nightmare.wait(1000 + getRandomInt(timer));
+    console.log(`Wait done for viewer ${id}`);
+  } catch (e) {
+    console.error(`Error for viewer ${id}:`, e);
+  }
+  await nightmare.end();
+  console.log(`End done for viewer ${id}`);
+}
+
+async function getViews(proxies) {
+  console.log(`Starting bot attack with ${proxies.length} proxies`);
+  const executor = [];
+  for (let i = 0; i < proxies.length; i += multiplicator) {
+    console.log(`Launching ${multiplicator} viewers, progress: ${i + 1}/${proxies.length}`);
+    for (let j = 0; j < multiplicator; j++) {
+      const index = i + j;
+      if (index < proxies.length) {
+        const proxy = proxies[index];
+        executor[index] = execView(proxy, index);
+      }
     }
-    console.log("end done " + id);
+    await Promise.all(executor.slice(i, i + multiplicator));
+    await new Promise(resolve => setTimeout(resolve, debit));
+  }
 }
 
-function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
-    })
-}
-
-async function getviews(proxies) {
-    console.log("start bot attack! proxies.lenght: ",proxies.length);
-    var executor = [];
-
-    for(let i = 0; i <= proxies.length; i += multiplicator) {
-        console.log("lunch: ", multiplicator);
-        for(let x = 0; x !== multiplicator; x++) {
-            console.log("progress: ", proxies.length + " / " + (i + x));
-            executor[i + x] = Nightmare({ show: true , switches: {
-                    'proxy-server':  proxies[i + x].ipAddress + ":" + proxies[i + x].port,
-                    'ignore-certificate-errors': true
-                }});
-            let nightmare = executor[i + x];
-            console.log("view with proxy: "+ proxies[i + x].ipAddress + ":" + proxies[i + x].port);
-            execview(nightmare, i + x)
-        }
-       await sleep(debit)
-    }
-}
+(async () => {
+  const proxies = await ProxyLists.getProxies(options);
+  console.log(`Found ${proxies.length} proxies`);
+  const selectedProxies = proxies.slice(0, views);
+  await getViews(selectedProxies);
+})();
